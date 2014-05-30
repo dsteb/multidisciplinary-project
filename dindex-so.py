d9eb82203ad5d23b949775fbfe224bc5631781d4
EC2 Default User <ec2-user@ip-172-31-26-239.eu-west-1.compute.internal>
2014-05-30 22:15:51 +0000

    D-index for stackoverflow.

--- dindex-so.py
+++ dindex-so.py
@@ -4,14 +4,30 @@ import sys
 import json
 
 def main():
+    percent = 7.5
     filename = sys.argv[1]
     f = open(filename, 'r')
     json_data = json.load(f)
     answerer = json_data['answerer']
     print json.dumps(answerer, indent=4)
-    for question in json_data['questions']:
-        print json.dumps(question, indent=4)
+    ar0 = answerer["total_score"]
+    norm = 0
+    num = 0
+    questions = json_data['questions']
+    questions = sorted(questions, key=lambda question: question['time_diff'])
+    for question in questions:
+        t = question["time_diff"]
+        qr = question["questioner_reputation"]
+        qs = question["question_score"]
+        as0 = question["answer_score"]
+        interest = pow(1+percent/100, t)
+        interest = t
+        norm += interest
+        num += (qr + qs + as0)/(num/norm + 1.0)*interest
         
-
+        print t, qr, qs, ar0, as0, num, norm
+        #print json.dumps(question, indent=4)
+        
+    print num/norm
 if __name__ == '__main__':
     main()
