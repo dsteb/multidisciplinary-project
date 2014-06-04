11df73aa98934a7ff2844a79604ed2a1de5cb5c8
EC2 Default User <ec2-user@ip-172-31-26-239.eu-west-1.compute.internal>
2014-06-04 08:46:39 +0000

    Parsing pages from stackoverflow.

--- all.py
+++ all.py
@@ -4,6 +4,7 @@ import urllib2
 from bs4 import BeautifulSoup
 import threading
 import json
+import logging
 import mylogging
 import stackoverflow as so
 import os
@@ -16,26 +17,29 @@ URL = 'http://stackoverflow.com'
 USE_THREADS = False 
 
 if __name__ == '__main__':
-    url = '{}/users?tab=reputation&filter=quarter'.format(URL)
-    text = open(url)
-    soup = BeautifulSoup(text)
-    user_info_blocks = soup.find_all(class_='user-info')
-    for user_info in user_info_blocks:
-        try:
-            details = user_info.find(class_='user-details')
-            url = '{}/{}'.format(URL, details.a['href'])
-            username = details.a['href'].split('/')[-1]
-            if os.path.isfile('stackoverflow/{}.json'.format(username)):
-                logging.info('Skip ' + username)
-                continue
-            if USE_THREADS:
-                logging.info('creating thread for url: {}'.format(url))
-                t = threading.Thread(target=so.process_user, args=(url,))
-                t.start()
-            else: 
-                logging.info(url)
-                so.process_user(url)
-        except Exception as e:
-            logging.error(e)
+    page = 60
+    while True:
+        url = '{}/users?tab=reputation&filter=all&page={}'.format(URL, page)
+        text = open(url)
+        soup = BeautifulSoup(text)
+        user_info_blocks = soup.find_all(class_='user-info')
+        for user_info in user_info_blocks:
+            try:
+                details = user_info.find(class_='user-details')
+                url = '{}/{}'.format(URL, details.a['href'])
+                username = details.a['href'].split('/')[-1]
+                if os.path.isfile('stackoverflow/{}.json'.format(username)):
+                    logging.info('Skip ' + username)
+                    continue
+                if USE_THREADS:
+                    logging.info('creating thread for url: {}'.format(url))
+                    t = threading.Thread(target=so.process_user, args=(url,))
+                    t.start()
+                else: 
+                    logging.info(url)
+                    so.process_user(url)
+            except Exception as e:
+                logging.error(e)
+        page += 1
 
         
